d9240cd82d8909d2cedf2f43f16a0fa23d0aba95
Guilherme Passos <gepassos06@gmail.com>
2024-08-01 09:22:15 -0300

    Ajuste de nacionalidade das marcas não obrigatorio e alteração no nome da model cor

--- core/models/cor.py
+++ core/models/cor.py
@@ -2,5 +2,10 @@ from django.db import models
 
 class Cor(models.Model):
     nome = models.CharField(max_length=100)
+    
     def __str__(self):
-        return self.nome
+        return f"{self.nome}"
+    
+    class Meta:
+        verbose_name = "Cor"
+        verbose_name_plural = "Cores"
